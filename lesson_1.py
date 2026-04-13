5a7ff7b0e94502a1341cfe5966180fbcd666eac9
Mashal Asif <mashalasif12@gmail.com>
2026-04-13 20:33:03 +0500

    Initial commit

---
+++ lesson_1.py
@@ -0,0 +1,43 @@
+'''random_number=67
+print(random_number)
+'''
+#Excersise:2
+'''
+astronaut_name="Mashal Asif"
+print(astronaut_name)
+astronaunt_age=21
+print(astronaunt_age)
+fuel=16.8
+print(fuel)
+is_engine_running=True
+print(is_engine_running)
+astronaunts=["Mashal Asif","Minahil","Maria","Aamir"]
+print(astronaunts)
+astro_ages={
+    "Mashal Asif":21,
+    "Minahil":22,
+    "Maria":20,
+    "Aamir":23
+}
+print(astro_ages)
+'''
+#Excersise:3
+'''
+Space-Resource Calculation
+'''
+speed = float(input("Enter speed of craft: "))
+time = float(input("Enter time: "))
+
+print("Speed of craft:", speed)
+print("Time to reach distance:", time)
+
+distance = speed / time
+fuel_eff = 89.98
+
+print("Distance:", distance)
+
+fuel_consump = distance / fuel_eff
+time_to_dest = distance / speed
+
+full_name = input("Enter full name: ")
+print("Full name:", full_name)
